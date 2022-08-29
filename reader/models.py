f4dccbd90b98fa74c23cc34d9e5064f9f30ca629
Jerin Saji <106364394+jerin-saji@users.noreply.github.com>
2022-08-29 10:50:11 +0530

    Add files via upload

--- reader/models.py
+++ reader/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Grade(models.Model):
-    code = models.CharField(max_length=20)
+    grade = models.CharField(max_length=20)
     def __str__(self):
-        return self.code
+        return self.grade
 
 
 class Student(models.Model):
--- reader/views.py
+++ reader/views.py
@@ -26,29 +26,52 @@ def upload_file(request):
                 messages.error(request,'Please insert Excel file')
                 return redirect('home')
             handle_uploaded_file(f)
-            # return HttpResponseRedirect('success/')
-            df = pd.read_excel('reader/destination/destination'+f.name)
             
-            # if not Student.objects.filter(id=data2[0]).exists():
-            #         obj = Student(id=data2[0], first_name=data2[1], last_name=data2[2], email=data2[3], gender=data2[4])
-            # col1=data1[0]
-            # if data1.loc[id] not in data2:
-            # data3 = data1.copy(deep=True)
-            # print(data1.columns)
-            # print(data1['id'])
-            # if  data3['id'] not in data1['id'] :
-            objs = list()
-            for index, row in df.iterrows():
-                grade = Grade.objects.filter(code=row["grade"]).values("id")
-                obj=Student(id=row["id"],first_name=row["first_name"],last_name=row["last_name"],email=row["email"],gender=row["gender"],grade_id =grade)
-                objs.append(obj)
+            # df = pd.read_excel('reader/destination/destination'+f.name)
+            df = pd.read_excel('reader/destination/destination'+f.name)
+            #--------------------------------------------------#--------------------------#
+            #new codes
+    
+            
+            
+            
+            
+            import sqlite3
 
-            # Student.objects.bulk_create(
-            # Student(**vals) for vals in data1.to_dict('records')
-            # )
-            Student.objects.bulk_create(objs)
+            connect2 = sqlite3.connect('db.sqlite3')
+            df2 = pd.read_sql_query('Select * from reader_grade;',connect2)
+            print(df2)
+            new_data=pd.merge(df,df2,on="grade")
+            print(new_data)
+            df_records = new_data.to_dict('records')
+            record_lists = [Student(
+                id = record['id_x'],
+                first_name=record['first_name'],
+                last_name = record['last_name'],
+                gender = record['gender'],
+                grade_id = record['id_y'],
+                ) for record in df_records]
+            
+            
 
-            object_list = Student.objects.all().values('id', 'first_name', 'last_name', 'email', 'gender', 'grade__code')
+            
+
+            #--------------------------------------------------#--------------------------#
+        
+            # objs = list()
+            # for index, row in df.iterrows():
+            #     grade = Grade.objects.filter(code=row["grade"]).values("id")
+            #     obj=Student(id=row["id"],first_name=row["first_name"],last_name=row["last_name"],email=row["email"],gender=row["gender"],grade_id =grade)
+            #     objs.append(obj)
+#--------------------------------------------#                             #----------------#
+
+            # Student.objects.bulk_create(objs)    #-------->(objs)
+
+            # object_list = Student.objects.all().values('id', 'first_name', 'last_name', 'email', 'gender', 'grade__code')
+            # return render(request,'reader/success.html',{'object_list':object_list})   
+            
+            Student.objects.bulk_create(record_lists) 
+            object_list = Student.objects.all().values('id', 'first_name', 'last_name', 'email', 'gender', 'grade__grade')
             return render(request,'reader/success.html',{'object_list':object_list})   
 
 
@@ -57,9 +80,6 @@ def upload_file(request):
     return render(request, 'reader/upload.html', {'form': form})
 
 
-# def success(request):
-    
-#     return render(request,'reader/success.html')
 
 
 
